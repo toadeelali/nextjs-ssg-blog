bf19065d7779bbae01a272e819bbc687ff729ddb
Adeel Ali <wikz@live.com>
2021-01-03 21:17:13 +0500

    feat complete tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link'
+import Date from '../components/date'
 
 
 export default function Home({ allPostsData }) {
-  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
@@ -24,11 +25,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
